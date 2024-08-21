2e2e3b70fcedd299928ddf5f7b3ca84e92812cd3
Tuan Nguyen <tuanng2210@gmail.com>
2024-08-21 16:03:47 -0400

    update account API

--- src/main/java/com/example/account_API/controller/AuthController.java
+++ src/main/java/com/example/account_API/controller/AuthController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.account_API.security.TokenService;
 
 @RestController
+@RequestMapping("/account")
 public class AuthController {
 
     @Autowired
